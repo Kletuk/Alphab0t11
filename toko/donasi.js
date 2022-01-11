35c5b9a715c80f03e82252be398f017fa269358c
Kletuk <95207384+Kletuk@users.noreply.github.com>
2022-01-12 03:29:09 +0700

    Update and rename shop/donasi.js to toko/donasi.js
    P

---
+++ toko/donasi.js
@@ -0,0 +1,18 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 089680887039
+2. OVO
+	ㅁ 089680887039
+3. DANA
+	ㅁ 089680887039
+	
+`
+	}
+
+exports.donasibot = donasibot
+ 
